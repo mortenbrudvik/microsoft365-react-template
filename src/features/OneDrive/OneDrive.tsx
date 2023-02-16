31ee92ffc818b5abdddeefb093b57ec93bc14c00
Morten Brudvik <morten@brudvik.com>
2023-02-16 12:55:41 +0100

    feat: Add breadcrumbs for better navigation

--- src/features/OneDrive/OneDrive.tsx
+++ src/features/OneDrive/OneDrive.tsx
@@ -1,22 +1,39 @@
 import * as React from 'react';
 import {FileList} from '@microsoft/mgt-react';
-import {Stack, Title} from "@mantine/core";
+import {Breadcrumbs, Stack, Title} from "@mantine/core";
 import {useState} from "react";
 import {DriveItem} from '@microsoft/microsoft-graph-types';
 
 export const OneDrive = () => {
     const [folderId, setFolderId] = useState('root');
+    const [breadcrumbs, setBreadcrumbs] = useState<DriveItem[]>([]);
 
     const handleItemClick = (event: CustomEvent) => {
         const item = event.detail as DriveItem;
         if (item.folder) {
             setFolderId(item.id!);
+            setBreadcrumbs([...breadcrumbs, item]);
         }
     };
+
+    function handleBreadcrumbClick(item: DriveItem) {
+        setFolderId(item.id!);
+        setBreadcrumbs(breadcrumbs.slice(0, breadcrumbs.indexOf(item) + 1));
+    }
 
     return (
         <Stack w={500}>
             <Title order={3}>OneDrive</Title>
+            <Breadcrumbs>
+                <a href="#" onClick={() => handleBreadcrumbClick({ id: 'root' })}>
+                    Home
+                </a>
+                {breadcrumbs.map((item) => (
+                    <a key={item.id} href="#" onClick={() => handleBreadcrumbClick(item)}>
+                        {item.name}
+                    </a>
+                ))}
+            </Breadcrumbs>
             <FileList itemId={folderId} itemClick={(e) => {
                 handleItemClick(e as CustomEvent);
             }}/>
